360440ecab1ffd5f8732a25acff9b688ca6a4664
Mital Joshi <tbs.mital@gmail.com>
2020-06-04 16:16:55 +0530

    added message list

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  currentEnable: number = 1;
+  inProgress: boolean = true;
+  constructor() { }
 
-  constructor() {}
-
+  checkDisability(status) {
+    return this.currentEnable == status ? false : true;
+  }
+  changeNext() {
+    if (this.currentEnable < 5) {
+      this.currentEnable = this.currentEnable + 1;
+    } else {
+      this.currentEnable = 1
+    }
+  }
+  getImageURL(status) {
+    if (this.currentEnable == status) {
+      return "./assets/animatcons/hourglass.gif";
+    } else if (this.currentEnable > status) {
+      return "./assets/animatcons/check.gif";
+    } else {
+      return "./assets/animatcons/activity.gif";
+    }
+  }
 }
